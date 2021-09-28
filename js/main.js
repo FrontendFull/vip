e1e6fb0f4ddf207242374a2b0fbf3e129e084705
YuraSergeevv <65806687+YuraSergeevv@users.noreply.github.com>
2021-09-29 00:18:21 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
 
-window.onload = function (){
-    let k=0;
-    for (const item of document.querySelectorAll('.master_item')) {
-        k++;
-        for (let i = 2; i <= 3; i++) {
-            item.insertAdjacentHTML('beforeend', `  <div class="img_warp none " data-fancybox="girl-${k}" data-src="img/girls/big/${k}-${i}.jpg"><div>`)
-            console.log(k,i)
+window.onload = function () {
+
+    let k = 0;
+    let i = 0;
+
+    // var img = new Image();
+    // img.src = 'img/girls/card/1.jpg';
+    // img.onload = function () {
+    //     check = true;
+    // };
+
+    // setTimeout(() => {
+    //     console.log(check);
+    // }, 0)
+
+
+
+    setTimeout(() => {
+        for (const item of document.querySelectorAll('.master_item')) {
+            k++;
+            setTimeout(() => {
+                i++;
+                var imageUrl = `img/girls/big/${k}/${i}.jpg`;
+                imageExists(imageUrl, function (exists) {
+                    if (exists) {
+                        console.log(1);
+                    }
+                    else {
+                        k++;
+                        i = 0;
+                    }
+                    console.log('RESULT: url=' + imageUrl + ', exists=' + exists);
+                });
+
+
+            }, 100)
+
         }
+    }, 100)
+
+    function imageExists(url, callback) {
+        var img = new Image();
+        img.onload = function () { callback(true); };
+        img.onerror = function () { callback(false); };
+        img.src = url;
     }
+
+    // Sample usage
+
+
+
+    // function loadImg(item, n) {
+    //     for (let m = 2; m <= 5; m++) {
+    //         let img = new Image();
+    //         img.src = `img/girls/big/${n}-${m}.jpg`;
+
+    //         img.onload = function () {
+    //             console.log(n, m);
+    //             console.log('true');
+    //             item.insertAdjacentHTML('beforeend', ` <div class="img_warp-${n}-${m} none "  data-fancybox="girl-${n}" data-src="img/girls/big/${n}-${m}.jpg"><div>`);
+    //         };
+    //         img.onerror = function () {
+    //             k++;
+    //             console.log(n, m);
+    //             console.log('false');
+    //         };
+    //     }
+
+    // }
 };
+
