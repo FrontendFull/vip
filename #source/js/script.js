80db7f9b8f58ceb2d2e92c782d3212e80f591fb4
yuras <work.sergeevv@gmail.com>
2021-09-28 18:55:51 +0300

    js fancybox

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,3 +1,10 @@
 window.onload = function (){
-
+    let k=0;
+    for (const item of document.querySelectorAll('.master_item')) {
+        k++;
+        for (let i = 2; i <= 3; i++) {
+            item.insertAdjacentHTML('beforeend', `  <div class="img_warp none " data-fancybox="girl-${k}" data-src="img/girls/big/${k}-${i}.jpg"><div>`)
+            console.log(k,i)
+        }
+    }
 };
